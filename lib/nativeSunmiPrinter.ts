779be079dfe5366ef253788497b5f48ef0bbcbce
Mohammad <mimhussainy@gmail.com>
2026-07-03 15:03:42 +0200

    new build

--- lib/nativeSunmiPrinter.ts
+++ lib/nativeSunmiPrinter.ts
@@ -2,7 +2,7 @@ import { NativeModules, Platform } from 'react-native';
 
 const { SunmiPrinterModule } = NativeModules;
 
-export async function printSunmiReceiptNative(order: any, restaurant: any): Promise<boolean> {
+export async function printSunmiInstructionsNative(instructions: any[]): Promise<boolean> {
   if (Platform.OS !== 'android') {
     return false;
   }
@@ -11,5 +11,5 @@ export async function printSunmiReceiptNative(order: any, restaurant: any): Prom
     throw new Error('SunmiPrinterModule is not registered in NativeModules. Check SunmiPrinterPackage registration in MainApplication and the config plugin.');
   }
 
-  return SunmiPrinterModule.printSunmiReceipt(JSON.stringify(order), JSON.stringify(restaurant));
+  return SunmiPrinterModule.printInstructions(JSON.stringify(instructions));
 }
